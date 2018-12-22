6ca52bf208dbabff61005554ed273b7d3a716b57
victor <victorzhangq@gmail.com>
2018-12-22 21:24:02 +0800

    Initialized

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # tornado_testsite
+
+A simple example on telling you how to config a tornado webapp project on CentOS. 
+
+Including /etc/nginx.conf, /etc/supervisor/tornado.conf
+
+Attention Please:
+If you restarted your cloud server, and the webapp failed to autostart, and then, you want to confirm that what's wrong with that? You entered the command 
+```
+supervisorctl status
+```
+and it gave that error message, like this "unix:///tmp/supervisor.sock refused connection", 
+so after searching google, the solution is 
+```
+sudo supervisord -c /etc/supervisord.conf   
+
+sudo supervisorctl status  # see is it running well?
+
+sudo service nginx restart # restart your nginx
+```
+
+https://stackoverflow.com/questions/20067116/supervisorctl-error-unix-var-run-supervisord-sock-refused-connection
+
---
+++ main.py
@@ -0,0 +1,30 @@
+import tornado.ioloop
+import tornado.web
+import os
+
+from tornado.options import define, options
+define("port", default=8100, help="run on the given port", type=int)
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("index.html")
+
+
+def make_app():
+    settings = dict(
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+    )
+    return tornado.web.Application(
+        [(r"/", MainHandler)], 
+        **settings
+    )
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = make_app()
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()#  
